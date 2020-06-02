39e15757bdf2685a93d6b5ae33a8422ec5c5033a
Chris Cairns <reg@monexa.co.uk>
2020-06-02 22:48:10 +0100

    Exercise 1

--- app.py
+++ app.py
@@ -6,7 +6,26 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    toDoItems = sorted(session.get_items(), key=lambda item: item['status'], reverse=True)
+    return render_template('index.html', toDoItems=toDoItems)
+
+@app.route('/', methods=['POST'])
+def add():
+    session.add_item(request.form['title'])
+    return redirect(url_for('index'))
+
+@app.route('/complete', methods=['POST'])
+def complete():
+    id = int(request.form['id'])
+    toDoItem = session.get_item(id)
+    toDoItem['status'] = 'Completed'
+    session.save_item(toDoItem)
+    return redirect(url_for('index'))
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    session.remove_item(int(request.form['id']))
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
