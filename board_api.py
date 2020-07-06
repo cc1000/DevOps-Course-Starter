8476ebb6c106342dc075d2c79eeb667fe431bed0
Chris Cairns <reg@monexa.co.uk>
2020-07-06 06:58:45 +0100

    Use params for GET querystring

--- board_api.py
+++ board_api.py
@@ -3,7 +3,12 @@ import trello_config
 from ToDoItem import ToDoItem
 
 def get_items():
-    response = requests.get(f'{trello_config.root_url}/boards/{trello_config.board_id}/cards?key={trello_config.api_key}&token={trello_config.token}')
+    params = {
+        'key': trello_config.api_key,
+        'token': trello_config.token
+    }
+
+    response = requests.get(f'{trello_config.root_url}/boards/{trello_config.board_id}/cards', params=params)
     response.raise_for_status()
     trello_cards = response.json()
     return [ToDoItem(card) for card in trello_cards]
@@ -26,7 +31,6 @@ def complete_item(id):
     }
     response = requests.put(f'{trello_config.root_url}/cards/{id}', data=payload)
     response.raise_for_status()
-
 
 def delete_item(id):
     response = requests.delete(f'{trello_config.root_url}/cards/{id}?key={trello_config.api_key}&token={trello_config.token}')
