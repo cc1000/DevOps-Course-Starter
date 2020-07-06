e5ad28b762021ed05724a62195766f4fbf29540d
Chris Cairns <reg@monexa.co.uk>
2020-07-06 06:51:02 +0100

    Clean up trello_config imports

--- ToDoItem.py
+++ ToDoItem.py
@@ -1,7 +1,7 @@
-from trello_config import trello_done_list_id
+from trello_config import done_list_id
 
 class ToDoItem:
     def __init__(self, trello_item_data):
         self.id = trello_item_data['id']
         self.title = trello_item_data['name']
-        self.status = 'Completed' if trello_item_data['idList'] == trello_done_list_id else 'To Do'
+        self.status = 'Completed' if trello_item_data['idList'] == done_list_id else 'To Do'
--- board_api.py
+++ board_api.py
@@ -1,33 +1,33 @@
 import requests
-from trello_config import trello_api_key, trello_token, trello_root_url, trello_board_id, trello_todo_list_id, trello_done_list_id
+import trello_config
 from ToDoItem import ToDoItem
 
 def get_items():
-    response = requests.get(f'{trello_root_url}/boards/{trello_board_id}/cards?key={trello_api_key}&token={trello_token}')
+    response = requests.get(f'{trello_config.root_url}/boards/{trello_config.board_id}/cards?key={trello_config.api_key}&token={trello_config.token}')
     response.raise_for_status()
     trello_cards = response.json()
     return [ToDoItem(card) for card in trello_cards]
 
 def add_item(title):
     payload = {
-        'key': trello_api_key,
-        'token': trello_token,
-        'idList': trello_todo_list_id,
+        'key': trello_config.api_key,
+        'token': trello_config.token,
+        'idList': trello_config.todo_list_id,
         'name': title
     }
-    response = requests.post(f'{trello_root_url}/cards', data=payload)
+    response = requests.post(f'{trello_config.root_url}/cards', data=payload)
     response.raise_for_status()
 
 def complete_item(id):
     payload = {
-        'key': trello_api_key,
-        'token': trello_token,
-        'idList': trello_done_list_id
+        'key': trello_config.api_key,
+        'token': trello_config.token,
+        'idList': trello_config.done_list_id
     }
-    response = requests.put(f'{trello_root_url}/cards/{id}', data=payload)
+    response = requests.put(f'{trello_config.root_url}/cards/{id}', data=payload)
     response.raise_for_status()
 
 
 def delete_item(id):
-    response = requests.delete(f'{trello_root_url}/cards/{id}?key={trello_api_key}&token={trello_token}')
+    response = requests.delete(f'{trello_config.root_url}/cards/{id}?key={trello_config.api_key}&token={trello_config.token}')
     response.raise_for_status()
