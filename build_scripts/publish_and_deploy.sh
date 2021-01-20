3662c561ef4d304d12277362185af441214d2c90
Chris Cairns <reg@monexa.co.uk>
2021-01-20 23:34:01 +0000

    Rename Heroku API key environment variable; use stdin for Heroku Docker login

--- build_scripts/publish_and_deploy.sh
+++ build_scripts/publish_and_deploy.sh
@@ -1,5 +1,5 @@
 docker push $1/todo-app:latest
 docker tag $1/todo-app:latest registry.heroku.com/cree1000-todo-app/web
-docker login --username=$2 --password=$3 registry.heroku.com
+echo $3 | docker login -u $2 --password-stdin registry.heroku.com
 docker push registry.heroku.com/cree1000-todo-app/web
 heroku container:release -a cree1000-todo-app web
