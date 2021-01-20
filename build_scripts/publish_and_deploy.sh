f38b2542799db6ba25f703a7a0f86141302ac974
Chris Cairns <reg@monexa.co.uk>
2021-01-20 23:23:36 +0000

    Add Heroku deployment

--- build_scripts/publish_and_deploy.sh
+++ build_scripts/publish_and_deploy.sh
@@ -1,1 +1,5 @@
 docker push $1/todo-app:latest
+docker tag $1/todo-app:latest registry.heroku.com/cree1000-todo-app/web
+docker login --username=$2 --password=$3 registry.heroku.com
+docker push registry.heroku.com/cree1000-todo-app/web
+heroku container:release -a cree1000-todo-app web
