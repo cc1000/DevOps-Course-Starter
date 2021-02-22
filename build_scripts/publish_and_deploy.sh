34044a78771bd8bb699b2ac360d6a1f5ab54cde3
Chris Cairns <reg@monexa.co.uk>
2021-02-22 20:15:24 +0000

    Move Docker publish to before_deploy stage

--- build_scripts/publish_and_deploy.sh
+++ build_scripts/publish_and_deploy.sh
@@ -1,5 +1,3 @@
-docker push $1/todo-app:latest
-docker tag $1/todo-app:latest registry.heroku.com/cree1000-todo-app/web
-echo $3 | docker login -u $2 --password-stdin registry.heroku.com
+echo $2 | docker login -u $1 --password-stdin registry.heroku.com
 docker push registry.heroku.com/cree1000-todo-app/web
 heroku container:release -a cree1000-todo-app web
