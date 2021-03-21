f9354f33c9599a2707999d67107dbe8b1c9daf7c
Chris Cairns <reg@monexa.co.uk>
2021-03-21 21:28:43 +0000

    Stub out LoginManager

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from board_repository import BoardRepository
 from to_do_items_view_model import ToDoItemsViewModel
+from flask_login import LoginManager, login_required
 
 def create_app():
     app = Flask(__name__)
     board_repository = BoardRepository()
+    login_manager = LoginManager()
 
     @app.route('/')
+    @login_required
     def index():
         to_do_items = sorted(board_repository.get_items(), key=lambda item: item.status, reverse=True)
         view_model = ToDoItemsViewModel(to_do_items)
@@ -26,6 +29,16 @@ def create_app():
     def delete_item():
         board_repository.delete_item(request.args.get('id'))
         return redirect(url_for('index'))
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return None
+
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        pass # TODO: Redirect to GitHub
+
+    login_manager.init_app(app)
 
     return app
 
